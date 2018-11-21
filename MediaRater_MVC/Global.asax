0fff44fd2a2b37482aae8cf5058af23a4320646f
zjmcdon98 <zjmcdon98@gmail.com>
2018-11-21 09:51:06 -0500

    Add project files.

---
+++ MediaRater_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MediaRater_MVC.MvcApplication" Language="C#" %>
---
+++ MediaRater_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MediaRater_MVC.Startup))]
+namespace MediaRater_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
