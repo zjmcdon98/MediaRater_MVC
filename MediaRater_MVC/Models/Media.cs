3d0ed348f94005a44a04895f181bf55bb237e0de
zjmcdon98 <zjmcdon98@gmail.com>
2018-11-21 10:36:04 -0500

    Edit

---
+++ MediaRater_MVC/Models/Media.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MediaRater_MVC.Models
+{
+    public class Media
+    {
+        public int MediaID { get; set; }
+        public string Name { get; set; }
+        public string MediaType { get; set; }
+        public string Rating { get; set; }
+    }
+
+    public class MediaDBContext : DbContext
+    {
+        public DbSet<Media> Media { get; set; }
+    }
+}
