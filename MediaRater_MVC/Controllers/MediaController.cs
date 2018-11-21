d06071eb0191a232ba6eeba89dd072a8bb8cc8b3
zjmcdon98 <zjmcdon98@gmail.com>
2018-11-21 13:23:22 -0500

    Edit

--- MediaRater_MVC/Controllers/MediaController.cs
+++ MediaRater_MVC/Controllers/MediaController.cs
@@ -17,5 +17,26 @@ namespace MediaRater_MVC.Controllers
         {
             return View(db.Media.ToList());
         }
-    }
+
+        // GET: Media/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Post: Media/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MediaID,Name")] Media media)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Media.Add(media);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(media);
+        }
+    }  
 }
